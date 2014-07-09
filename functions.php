e1a453751a87cba74ac549c93add6444589062e1
jamesrtyler <jtyler@JTYLERMLMB.local>
2014-07-09 10:13:30 -0500

    Add schema markup to navigation menu.
    Added schema markup to navigation menu using a custom walker.

--- functions.php
+++ functions.php
@@ -81,12 +81,130 @@ function html5blank_nav()
 		'after'           => '',
 		'link_before'     => '',
 		'link_after'      => '',
-		'items_wrap'      => '<ul>%3$s</ul>',
+		'items_wrap'      => '<ul itemscope=itemscope itemtype="http://www.schema.org/SiteNavigationElement">%3$s</ul>',
 		'depth'           => 0,
-		'walker'          => ''
+		'walker'          => new schema_walker_nav_menu
 		)
 	);
 }
+
+
+// Custom Walker to add schema markup to the li and a element.
+
+class schema_walker_nav_menu extends Walker_Nav_Menu {
+	/**
+	 * Start the element output.
+	 *
+	 * @see Walker::start_el()
+	 *
+	 * @since 3.0.0
+	 *
+	 * @param string $output Passed by reference. Used to append additional content.
+	 * @param object $item   Menu item data object.
+	 * @param int    $depth  Depth of menu item. Used for padding.
+	 * @param array  $args   An array of arguments. @see wp_nav_menu()
+	 * @param int    $id     Current item ID.
+	 */
+	function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
+		$indent = ( $depth ) ? str_repeat( "\t", $depth ) : '';
+
+		$class_names = '';
+
+		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
+		$classes[] = 'menu-item-' . $item->ID;
+
+		/**
+		 * Filter the CSS class(es) applied to a menu item's <li>.
+		 *
+		 * @since 3.0.0
+		 *
+		 * @see wp_nav_menu()
+		 *
+		 * @param array  $classes The CSS classes that are applied to the menu item's <li>.
+		 * @param object $item    The current menu item.
+		 * @param array  $args    An array of wp_nav_menu() arguments.
+		 */
+		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
+		$class_names = $class_names ? ' class="' . esc_attr( $class_names ) . '"' : '';
+
+		/**
+		 * Filter the ID applied to a menu item's <li>.
+		 *
+		 * @since 3.0.1
+		 *
+		 * @see wp_nav_menu()
+		 *
+		 * @param string $menu_id The ID that is applied to the menu item's <li>.
+		 * @param object $item    The current menu item.
+		 * @param array  $args    An array of wp_nav_menu() arguments.
+		 */
+		$id = apply_filters( 'nav_menu_item_id', 'menu-item-'. $item->ID, $item, $args );
+		$id = $id ? ' id="' . esc_attr( $id ) . '"' : '';
+
+		$output .= $indent . '<li itemprop=name' . $id . $class_names .'>';
+
+		$atts = array();
+		$atts['title']  = ! empty( $item->attr_title ) ? $item->attr_title : '';
+		$atts['target'] = ! empty( $item->target )     ? $item->target     : '';
+		$atts['rel']    = ! empty( $item->xfn )        ? $item->xfn        : '';
+		$atts['href']   = ! empty( $item->url )        ? $item->url        : '';
+
+		/**
+		 * Filter the HTML attributes applied to a menu item's <a>.
+		 *
+		 * @since 3.6.0
+		 *
+		 * @see wp_nav_menu()
+		 *
+		 * @param array $atts {
+		 *     The HTML attributes applied to the menu item's <a>, empty strings are ignored.
+		 *
+		 *     @type string $title  Title attribute.
+		 *     @type string $target Target attribute.
+		 *     @type string $rel    The rel attribute.
+		 *     @type string $href   The href attribute.
+		 * }
+		 * @param object $item The current menu item.
+		 * @param array  $args An array of wp_nav_menu() arguments.
+		 */
+		$atts = apply_filters( 'nav_menu_link_attributes', $atts, $item, $args );
+
+		$attributes = '';
+		foreach ( $atts as $attr => $value ) {
+			if ( ! empty( $value ) ) {
+				$value = ( 'href' === $attr ) ? esc_url( $value ) : esc_attr( $value );
+				$attributes .= ' ' . $attr . '="' . $value . '"';
+			}
+		}
+
+		$item_output = $args->before;
+		$item_output .= '<a itemprop=url'. $attributes .'>';
+		/** This filter is documented in wp-includes/post-template.php */
+		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
+		$item_output .= '</a>';
+		$item_output .= $args->after;
+
+		/**
+		 * Filter a menu item's starting output.
+		 *
+		 * The menu item's starting output only includes $args->before, the opening <a>,
+		 * the menu item's title, the closing </a>, and $args->after. Currently, there is
+		 * no filter for modifying the opening and closing <li> for a menu item.
+		 *
+		 * @since 3.0.0
+		 *
+		 * @see wp_nav_menu()
+		 *
+		 * @param string $item_output The menu item's starting HTML output.
+		 * @param object $item        Menu item data object.
+		 * @param int    $depth       Depth of menu item. Used for padding.
+		 * @param array  $args        An array of wp_nav_menu() arguments.
+		 */
+		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
+	}
+
+} // Walker_Nav_Menu
+
 
 // Load HTML5 Blank scripts (header.php)
 function html5blank_header_scripts()
@@ -794,4 +912,6 @@ function testimonials($atts, $content = null) {
 add_shortcode('testimonials', 'testimonials');
 
 // THE SHORTCODE ----> [testimonials words="news" more="View More" posts="5" category=""]
+
+
 ?>
